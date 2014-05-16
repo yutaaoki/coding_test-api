62967c6296c87b13d62ccc02fd23ffd9ebf375e0
yuta <y_aoki@ubicast.com>
2014-05-16 05:23:02 +0000

    add authetification.

--- lib/coding_test/api.rb
+++ lib/coding_test/api.rb
@@ -5,7 +5,18 @@ module CodingTest
     format :json
 
     get do
-      { "message" => "Hola!" }
+      { "message" => "Welcome to CodingTest Api" }
+    end
+
+    resource :sessions do
+      
+      http_digest({realm: 'CodingTest Api', opaque: 'Soca tun up!' }) do |username, password|
+        { 'test' => 'test' }[username]
+      end
+
+      get do
+        'Work on meh!'
+      end
     end
   end
 end
