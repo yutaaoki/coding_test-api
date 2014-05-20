ef90d2f5760ed19922005c553eed2b5606cec8c2
yuta <y_aoki@ubicast.com>
2014-05-20 09:05:35 +0000

    more test

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -7,6 +7,14 @@ require 'mongo'
 describe CodingTest::API do
   include Rack::Test::Methods
 
+  def app
+    CodingTest::API
+  end
+
+  def auth
+    digest_authorize 'test', 'test'
+  end
+
   Host = 'localhost'
   DBName = 'coding_test'
 
@@ -22,13 +30,6 @@ describe CodingTest::API do
     tests.remove("name" => /spec.+/)
   end
 
-  def app
-    CodingTest::API
-  end
-
-  def auth
-    digest_authorize 'test', 'test'
-  end
 
   describe "Auth" do
     it "without credentials" do
@@ -48,36 +49,37 @@ describe CodingTest::API do
   end
 
   describe "Operation returing 200:" do
-    it "GET tests" do
+    before(:each) do
       auth
+    end
+    it "GET tests" do
       get "tests"
       last_response.status.should == 200
     end
     it "GET tests/spec_empty" do
-      auth
       get 'tests/spec_empty'
       last_response.status.should == 200
     end
     it "PUT tests/spec_put_200" do
-      auth
       data = {'name' => 'spec_put_test', 'introduction' => 'sample test', 'codes' => [1 => 'some code']}
       put "tests/spec_put_200", {'data' => data.to_json}
       last_response.status.should == 200
     end
     it "DELETE tests/spec_empty"  do
-      auth
       delete 'tests/spec_empty'
       last_response.status.should == 200
     end
   end
 
   describe "Integrated operation" do
+    before(:each) do
+      auth
+    end
     it "puts, gets, delets, and verifies a test" do
       name = 'spec_put_get'
-      data = {'name' => name, 'introduction' => 'Put your hands in the air!', 'codes' => [1 => 'some code']}
-      auth
+      data = {name: name, introduction: 'Put your hands in the air!', codes: [1 => 'some code']}
       #put
-      put "tests/#{name}", {'data' => data.to_json}
+      put "tests/#{name}", {data: data.to_json}
       last_response.status.should == 200
       #verify
       get "tests/#{name}"
@@ -92,7 +94,25 @@ describe CodingTest::API do
       last_response.status.should == 200
       expect(last_response.body).to eq("null")
     end
+    it "puts a lot of tests" do
+      def hundred
+        name = "spec_put_num"
+        (1..100).each do |num|
+          iname = name+num.to_s
+          data = {name: iname, introduction: 'Put your hands in the air!', codes: [1 => 'some code']}
+          yield iname, data
+          expect(last_response.status).to eq(200)
+        end
+      end
+      hundred do |iname, data|
+        put "tests/#{iname}", {data: data.to_json}
+      end
+      hundred do |iname, data|
+        get "tests/#{iname}"
+        res = JSON.parse(last_response.body)
+        expect(res["name"]).to eq(iname)
+      end
+    end
   end
-  
 end
 
