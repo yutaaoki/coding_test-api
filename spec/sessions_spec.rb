a6aa65fb02a4cef520f8514bb61e4b53163ba95d
yuta <y_aoki@ubicast.com>
2014-05-21 07:48:50 +0000

    more sessions test

--- spec/sessions_spec.rb
+++ spec/sessions_spec.rb
@@ -9,8 +9,36 @@ describe CodingTest::API do
   include Rack::Test::Methods
   include_context :api_test_context
 
+  def res200
+    expect(last_response.status).to eq(200)
+  end
+
   describe "Auth" do
     include_examples :auth_tests, "sessions"
+  end
+
+  describe "sessions" do
+
+    before(:each) do
+      auth
+    end
+
+    it "GET sessions" do
+      get "sessions"
+      res200
+    end
+
+    it "POST sessions" do
+      data = {name: 'spec_session_put', introduction: 'Put your hands in the air!', codes: [1 => 'some code']}
+      post "sessions", {data: data.to_json}
+      expect(last_response.status).to eq(201)
+    end
+
+    it "DELETE sessions" do
+      delete "sessions/spec_sessions"
+      res200
+    end
+
   end
 
 end
