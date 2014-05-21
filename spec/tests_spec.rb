a279f0290d53942f199ab56278da0be7cd983666
yuta <y_aoki@ubicast.com>
2014-05-21 07:49:52 +0000

    transpec

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -19,20 +19,20 @@ describe CodingTest::API do
     end
     it "GET tests" do
       get "tests"
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
     end
     it "GET tests/spec_empty" do
       get 'tests/spec_empty'
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
     end
     it "PUT tests/spec_put_200" do
       data = {'name' => 'spec_put_test', 'introduction' => 'sample test', 'codes' => [1 => 'some code']}
       put "tests/spec_put_200", {'data' => data.to_json}
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
     end
     it "DELETE tests/spec_empty"  do
       delete 'tests/spec_empty'
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
     end
   end
 
@@ -45,18 +45,18 @@ describe CodingTest::API do
       data = {name: name, introduction: 'Put your hands in the air!', codes: [1 => 'some code']}
       #put
       put "tests/#{name}", {data: data.to_json}
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
       #verify
       get "tests/#{name}"
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
       res = JSON.parse(last_response.body)
       expect(res['name']).to eq(name)
       #delte
       delete "tests/#{name}"
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
       #verify
       get "tests/#{name}"
-      last_response.status.should == 200
+      expect(last_response.status).to eq(200)
       expect(last_response.body).to eq("null")
     end
     it "puts a lot of tests" do
