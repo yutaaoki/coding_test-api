017f7447b1a35c41ecba600443e79c7099d16d82
yuta <y_aoki@ubicast.com>
2014-06-05 07:00:15 +0000

    user helpers

--- lib/coding_test/api.rb
+++ lib/coding_test/api.rb
@@ -4,6 +4,7 @@ require_relative 'data_access'
 module CodingTest
   class API < Grape::API
     format :json
+    helpers DataAccess
 
 
     get do
@@ -17,7 +18,7 @@ module CodingTest
     resource :sessions do
 
       get ':id' do
-        data = DataAccess::get_session(params[:id])
+        data = get_session(params[:id])
         if data == nil
           error! 'Session Not Exist', 404
         elsif data['started']
@@ -28,7 +29,7 @@ module CodingTest
       end
 
       post ':id' do
-        data = DataAccess::get_session(params[:id])
+        data = get_session(params[:id])
         if data == nil
           error! 'Session Not Exist', 404
         elsif data['started']
@@ -36,44 +37,44 @@ module CodingTest
         else
           time = Time.now.to_i
           data['started'] = time
-          DataAccess::update_session data['_id'], data
+          update_session data['_id'], data
           {location: "sessions/#{params[:id]}/content"}
         end
       end
 
+      helpers do
+        def if_started
+          data = get_session(params[:id])
+          if data == nil
+            error! 'Session Not Exist', 404
+          elsif data['started']
+            yield data
+          elsif data['finished']
+            error! 'Session Already Finished', 404
+          else
+            error! 'Session Not Started', 404
+          end
+        end
+      end
+
       get ':id/time' do
-        data = DataAccess::get_session(params[:id])
-        if data == nil
-          error! 'Session Not Exist', 404
-        elsif data['started']
+        if_started do |data|
           time = Time.new(data['started'].to_i)
           dif = Time.now - time
           {remaining: dif/60}
-        else
-          error! 'Session Not Started', 404
         end
       end
 
       put ':id/time' do
-        data = DataAccess::get_session(params[:id])
-        if data == nil
-          error! 'Session Not Exist', 404
-        elsif data['started']
+        if_started do |data|
           data['finished'] = 'true'
-          DataAccess::update_session data['_id'], data
-        else
-          error! 'Session Not Started', 404
+          update_session data['_id'], data
         end
       end
 
       get ':id/content' do
-        data = DataAccess::get_session(params[:id])
-        if data == nil
-          error! 'Session Not Exist', 404
-        elsif data['started']
+        if_started do |data|
           data
-        else
-          error! 'Session Not Started', 404
         end
       end
     end
@@ -89,7 +90,7 @@ module CodingTest
     resource :sessions do
 
       get do
-        DataAccess::get_sessions
+        get_sessions
       end
 
       params do
@@ -99,12 +100,12 @@ module CodingTest
         data = JSON.parse(params[:data])
         # add uuid
         data['_id'] = SecureRandom.uuid
-        DataAccess::insert_session data
+        insert_session data
         data
       end
 
       delete ':id' do
-        DataAccess::delete_session params[:id]
+        delete_session params[:id]
       end
     end
 
@@ -112,7 +113,7 @@ module CodingTest
     resource :tests do
 
       get do
-        DataAccess::all_tests
+        all_tests
       end
 
       params do
@@ -120,15 +121,15 @@ module CodingTest
         requires :data, type: String, desc: "Test data."
       end
       put ':id' do
-        DataAccess::update_test params[:id], params[:data]
+        update_test params[:id], params[:data]
       end
 
       get ':id' do
-        DataAccess::get_test params[:id]
+        get_test params[:id]
       end
 
       delete ':id' do
-        DataAccess::delete_test params[:id]
+        delete_test params[:id]
       end
     end
 
